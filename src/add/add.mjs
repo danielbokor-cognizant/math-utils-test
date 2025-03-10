8e79fdb423239afa196a0ef6b6aee72aaaea693a
Daniel Bokor <daniel.bokor@cognizant.com>
2025-03-10 21:19:50 +0200

    Using spinners with ora; Bumped to 1.3.0

--- src/add/add.mjs
+++ src/add/add.mjs
@@ -1,5 +1,22 @@
 import chalk from "chalk";
 
+let ora;
+
+try {
+  ora = await import("ora");
+} catch (e) {
+  console.log("ora not installed; proceeding without spinners");
+}
+
+export function showSpinner() {
+  if (ora) {
+    const spinner = ora.default("processing...").start();
+    startTimeout(() => spinner.succeed("Done!"), 1000);
+  } else {
+    console.log("processing...done");
+  }
+}
+
 export function add(a, b) {
   console.log(chalk.green("adding in ESM"));
   return a + b;
